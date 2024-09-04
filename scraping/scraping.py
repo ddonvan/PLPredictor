af1549a152c85c4ab339ad1150e79fa7c19721b9
ddonvan <donovand9@mymacewan.ca>
2024-09-04 14:22:31 -0600

    read scores table

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,14 +1,20 @@
-import requests
+import requests, pandas as pd
+from bs4 import BeautifulSoup
 
 standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 data = requests.get(standings_url)
 
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(data.text)
+soup = BeautifulSoup(data.text, 'html.parser')
 standings_table = soup.select('table.stats_table')[0]
 # get squad links
 links = standings_table.find_all('a')
 links = [l.get("href") for l in links]
 links = [l for l in links if '/squad' in l]
+
+team_urls = [f"https://fbref.com{l}" for l in links]
+team_url = team_urls[0]
+data = requests.get(team_url)
+
+matches = pd.read_html(data.text, match="Scores & Fixtures")
+print(matches)
 
