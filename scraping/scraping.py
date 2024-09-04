b8f58ce97e2c97adf31fa656038f7ac7cbc8c382
ddonvan <donovand9@mymacewan.ca>
2024-09-04 11:25:38 -0600

    get squad urls

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,1 +1,14 @@
 import requests
+
+standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+data = requests.get(standings_url)
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(data.text)
+standings_table = soup.select('table.stats_table')[0]
+# get squad links
+links = standings_table.find_all('a')
+links = [l.get("href") for l in links]
+links = [l for l in links if '/squad' in l]
+
