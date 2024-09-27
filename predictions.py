ecc8e8061bd4a25dfc39b9b292cf72416649b0d3
ddonvan <donovand9@mymacewan.ca>
2024-09-27 09:47:00 -0600

    implementation of ml model using one teams data

---
+++ predictions.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+matches = pd.read_csv("matches.csv", index_col=0)
+matches["date"] = pd.to_datetime(matches["date"])
+
+matches["venue_code"] = matches["venue"].astype("category").cat.codes
+matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
+matches["day_code"] = matches["date"].dt.dayofweek
+
+matches["target"] = (matches["result"] == "W").astype("int")
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+train = matches[matches["date"] < '2024-01-01']
+test = matches[matches["date"] > '2024-01-01']
+predictors = ["venue_code", "opp_code", "hour", "day_code"]
+rf.fit(train[predictors], train["target"])
+
+preds = rf.predict(test[predictors])
+from sklearn.metrics import accuracy_score, precision_score
+
+acc = accuracy_score(test["target"], preds)
+combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
+
+grouped_matches = matches.groupby("team")
+
+group = grouped_matches.get_group("Manchester City")
+
+def rolling_averages(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(3, closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+
+matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x, cols, new_cols))
+matches_rolling = matches_rolling.droplevel('team')
+matches_rolling.index = range(matches_rolling.shape[0])
+
+def make_predicitons(data, predictors):
+    train = data[data["date"] < '2024-01-01']
+    test = data[data["date"] > '2024-01-01']
+    rf.fit(train[predictors], train["target"])
+    preds = rf.predict(test[predictors])
+    combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
+    precision = precision_score(test["target"], preds)
+    return combined, precision
+
+combined, precision = make_predicitons(matches_rolling, predictors + new_cols)
+
+combined = combined.merge(matches_rolling[["date", "team", "opponent", "result"]], left_index=True, right_index=True)
+print(combined)
+
+ 
